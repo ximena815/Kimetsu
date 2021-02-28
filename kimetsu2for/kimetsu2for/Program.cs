4abf3e25d571c4353783b90b551cf19ba68d2269
ximena815 <68305685+ximena815@users.noreply.github.com>
2021-02-28 15:23:14 -0500

    Add files via upload

---
+++ kimetsu2for/kimetsu2for/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace kimetsu2for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nombres = { "Nezuko", "Tanjiro", "Zenitsu", "Inosuke", "Genya", "Kanao", "Tomioka" };
+            double[] estatura = { 153, 165, 164.5, 164, 180, 156, 176 };
+            Console.WriteLine("Kimetsu estaruras");
+            //-----------------------------------------------------
+            double total = 0;
+            for (int i = 0; i < estatura.Length; i++)
+            {
+                total += estatura[i];
+            }
+            Console.WriteLine("Total: " + total);
+            double promedio = total / estatura.Length;
+            Console.WriteLine("promedio: " + promedio);
+            //-----------------------------------------------------|X| = sqrt(x^2)
+            double[] distancias = new double[estatura.Length];
+            double min = 200;
+            int indice = 0;
+            //encontrar el nombre de la persona con estatura más parecida al promedio(menor distancia)
+            for (int i = 0; i < distancias.Length; i++)
+            {
+                distancias[i] =Math.Abs(estatura[i] - promedio);
+                if (distancias[i] < min)
+                {
+                    min = distancias[i];
+                    indice = i;
+                }
+            }
+            Console.WriteLine("Nombre de la persona aja: " + nombres[indice]);
+            //-----------------------------------------------------
+        }
+    }
+}
